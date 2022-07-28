99423a724759056eec8b013c24581f737341b47f
P3dro-Monteiro <pfernandesmonteiro@gmail.com>
2022-07-28 13:31:22 +0100

    Adds Docs folder and creates Position class

--- src/main/java/chess/App.java
+++ src/main/java/chess/App.java
@@ -1,13 +1,15 @@
 package chess;
 
+import chess.boardgame.Position;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
+        Position position = new Position(3, 5);
+        System.out.println(position);
     }
 }
